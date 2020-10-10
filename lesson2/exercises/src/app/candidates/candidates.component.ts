1f144626244324fc313b395ec04b3b65266e37e0
Angela Mitchell <anglarenmit@gmail.com>
2020-10-10 18:50:27 -0500

    Completed Exercise and Bonus

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -24,9 +24,15 @@ export class CandidatesComponent implements OnInit {
   }
 
   // Code the addToCrew function here:
-
+  addToCrew(candidate) {
+    if(!this.crew.includes(candidate)) {
+      this.crew.push(candidate);
+    }
+  }
 
   // BONUS: Code the changeMissionName function here:
-
+  changeMissionName(name) {
+    this.missionName = name;
+  }
 
 }
