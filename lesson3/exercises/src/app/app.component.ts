071d3148ec7715386cfc8533506920a518eb6c92
Angela Mitchell <anglarenmit@gmail.com>
2020-10-22 00:03:13 -0500

    Implemented dynamic enable/disable feature for direction buttons

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -14,6 +14,11 @@ export class AppComponent {
   message = 'Space shuttle ready for takeoff!';
   takeOffEnabled: boolean = true;
 
+  leftEnabled: boolean = true;
+  rightEnabled: boolean = true;
+  downEnabled: boolean = true;
+  upEnabled: boolean = true;
+
   handleTakeOff() {
     let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
     if (result) {
@@ -55,29 +60,43 @@ export class AppComponent {
       rocketImage.style.left = movement;
       this.width = this.width + 10000;
       this.color = 'blue';
+      this.leftEnabled = true;
     } else if (direction === 'left' && this.width > -20000) {
       let movement = parseInt(rocketImage.style.left) - 10 + 'px';
       rocketImage.style.left = movement;
       this.width = this.width - 10000;
-      this.color = 'blue'
+      this.color = 'blue';
+      this.rightEnabled = true;
     } else if (direction === 'up' && this.height < 340000) {
       let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
       rocketImage.style.bottom = movement;
       this.height = this.height + 10000;
       this.color = 'blue';
+      this.downEnabled = true;
     } else if (direction === 'down' && this.height > 0) {
       let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
       rocketImage.style.bottom = movement;
       this.height = this.height - 10000;
       this.color = 'blue';
+      this.upEnabled = true;
     }
   }
 
-  handleOutOfBounds() {
+  handleOutOfBounds(direction) {
     if (this.width <= -20000 || this.width >= 470000) {
       this.color = 'orange';
+      if (direction === 'left') {
+        this.leftEnabled = false;
+      } else if (direction === 'right') {
+        this.rightEnabled = false;
+      }
     } else if (this.height === 0 || this.height >= 340000) {
       this.color = 'orange';
+      if (direction === 'down') {
+        this.downEnabled = false;
+      } else if (direction === 'up') {
+        this.upEnabled = false;
+      }
     }
   }
 }
